759415d879172a9664883492d3d2c88dff8fc3da
Samiksha Wankar <94747339+Samiksha-Wankar@users.noreply.github.com>
2024-08-09 00:05:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
   console.log('Connected to MongoDB');
